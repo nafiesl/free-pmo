0ed6bd8dccc03ed3f3d3d78b1b760f9ae39c292b
Nafies Luthfi <nafiesl@gmail.com>
2017-11-19 22:03:42 +0800

    Update some config file to adopt laravel 5.5 config files

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,10 @@ return [
     |
     */
 
-    'from' => ['address' => env('MAIL_FROM', 'mail@app.dev'), 'name' => env('MAIL_NAME', 'Laravel')],
+    'from' => [
+        'address' => env('MAIL_FROM', 'mail@app.dev'),
+        'name'    => env('MAIL_NAME', 'Laravel'),
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -82,17 +85,6 @@ return [
 
     'username' => env('MAIL_USERNAME'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | SMTP Server Password
-    |--------------------------------------------------------------------------
-    |
-    | Here you may set the password required by your SMTP server to send out
-    | messages from your application. This will be given to the server on
-    | connection so that the application will be able to send messages.
-    |
-    */
-
     'password' => env('MAIL_PASSWORD'),
 
     /*
@@ -107,5 +99,24 @@ return [
     */
 
     'sendmail' => '/usr/sbin/sendmail -bs',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Markdown Mail Settings
+    |--------------------------------------------------------------------------
+    |
+    | If you are using Markdown based email rendering, you may configure your
+    | theme and component paths here, allowing you to customize the design
+    | of the emails. Or, you may simply stick with the Laravel defaults!
+    |
+    */
+
+    'markdown' => [
+        'theme' => 'default',
+
+        'paths' => [
+            resource_path('views/vendor/mail'),
+        ],
+    ],
 
 ];

e6efa10752e8f116e3be50c8e0d442eb4a2e7e3e
Nafies Luthfi <nafiesl@gmail.com>
2018-08-05 21:03:20 +0800

    Add comment time_display attribute
    Comment time will shown in diffForHumans if created today
    Comment time will shown in date time yesterday or before
    Add Comment model test

---
+++ tests/Unit/Models/CommentTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use Tests\TestCase;
+use App\Entities\Users\User;
+use App\Entities\Projects\Comment;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CommentTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_comment_has_belongs_to_creator_relation()
+    {
+        $comment = factory(Comment::class)->make();
+
+        $this->assertInstanceOf(User::class, $comment->creator);
+        $this->assertEquals($comment->creator_id, $comment->creator->id);
+    }
+
+    /** @test */
+    public function a_comment_has_time_display_attribute()
+    {
+        $comment = factory(Comment::class)->create(['created_at' => now()->subHour()]);
+        $this->assertEquals($comment->created_at->diffForHumans(), $comment->time_display);
+
+        $comment = factory(Comment::class)->create(['created_at' => now()->subDays(3)]);
+        $this->assertEquals($comment->created_at, $comment->time_display);
+    }
+}

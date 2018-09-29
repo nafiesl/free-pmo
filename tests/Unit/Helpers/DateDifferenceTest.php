33cf49f7e564bf137f57254d93bb4ea6314c6ad7
Nafies Luthfi <nafiesl@gmail.com>
2018-09-29 23:29:38 +0800

    Add more test to dateDifference helper function

--- tests/Unit/Helpers/DateDifferenceTest.php
+++ tests/Unit/Helpers/DateDifferenceTest.php
@@ -16,4 +16,23 @@ class DateDifferenceTest extends TestCase
     {
         $this->assertTrue(function_exists('dateDifference'));
     }
+
+    /** @test */
+    public function date_difference_returns_days_count_by_default()
+    {
+        $this->assertEquals(9, dateDifference('2018-04-01', '2018-04-10'));
+    }
+
+    /** @test */
+    public function date_difference_can_returns_formatted_string()
+    {
+        $this->assertEquals('9 days', dateDifference('2018-04-01', '2018-04-10', '%a days'));
+    }
+
+    /** @test */
+    public function date_difference_returns_proper_months_and_days_format()
+    {
+        // TODO: Need to fix, this should returns 1 months 9 days
+        $this->assertEquals('1 months 12 days', dateDifference('2018-03-01', '2018-04-10', '%m months %d days'));
+    }
 }

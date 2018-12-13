0367fdce7550641f42a79bb67177969c3f4b30b9
Nafies Luthfi <nafiesl@gmail.com>
2018-12-13 08:54:36 +0800

    Update db backups view labels
    Change @lang to __() helper

--- resources/views/backups/index.blade.php
+++ resources/views/backups/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
-@section('title',trans('backup.index_title'))
+@section('title', __('backup.index_title'))
 
 @section('content')
 <ul class="breadcrumb hidden-print">
-    <li><a href="{{ route('backups.index') }}">@lang('backup.index_title')</a></li>
-    <li class="active">@lang('backup.list')</li>
+    <li><a href="{{ route('backups.index') }}">{{ __('backup.index_title') }}</a></li>
+    <li class="active">{{ __('backup.list') }}</li>
 </ul>
 
 <div class="row">
@@ -13,11 +13,11 @@
         <div class="panel panel-default">
             <table class="table table-condensed">
                 <thead>
-                    <th>@lang('app.table_no')</th>
-                    <th>@lang('backup.file_name')</th>
-                    <th>@lang('backup.file_size')</th>
-                    <th>@lang('backup.created_at')</th>
-                    <th class="text-center">@lang('backup.actions')</th>
+                    <th>{{ __('app.table_no') }}</th>
+                    <th>{{ __('backup.file_name') }}</th>
+                    <th>{{ __('backup.file_size') }}</th>
+                    <th>{{ __('backup.created_at') }}</th>
+                    <th class="text-center">{{ __('backup.actions') }}</th>
                 </thead>
                 <tbody>
                     @forelse($backups as $key => $backup)
@@ -30,21 +30,19 @@
                             <a href="{{ route('backups.index', ['action' => 'restore', 'file_name' => $backup->getFilename()]) }}"
                                 id="restore_{{ str_replace('.gz', '', $backup->getFilename()) }}"
                                 class="btn btn-warning btn-xs"
-                                title="@lang('backup.download')"><i class="fa fa-rotate-left"></i></a>
+                                title="{{ __('backup.download') }}"><i class="fa fa-rotate-left"></i></a>
                             <a href="{{ route('backups.download', [$backup->getFilename()]) }}"
                                 id="download_{{ str_replace('.gz', '', $backup->getFilename()) }}"
                                 class="btn btn-info btn-xs"
-                                title="@lang('backup.download')"><i class="fa fa-download"></i></a>
+                                title="{{ __('backup.download') }}"><i class="fa fa-download"></i></a>
                             <a href="{{ route('backups.index', ['action' => 'delete', 'file_name' => $backup->getFilename()]) }}"
                                 id="del_{{ str_replace('.gz', '', $backup->getFilename()) }}"
                                 class="btn btn-danger btn-xs"
-                                title="@lang('backup.delete')"><i class="fa fa-remove"></i></a>
+                                title="{{ __('backup.delete') }}"><i class="fa fa-remove"></i></a>
                         </td>
                     </tr>
                     @empty
-                    <tr>
-                        <td colspan="3">@lang('backup.empty')</td>
-                    </tr>
+                    <tr><td colspan="5">{{ __('backup.empty') }}</td></tr>
                     @endforelse
                 </tbody>
             </table>

513b84835b1962a052b3c021526f84bd378d5372
Nafies Luthfi <nafiesl@gmail.com>
2019-01-09 21:00:06 +0800

    Change report routes to newer route style

--- routes/web/reports.php
+++ routes/web/reports.php
@@ -4,12 +4,15 @@ Route::group(['middleware' => ['web', 'role:admin'], 'prefix' => 'reports'], fun
     /*
      * Reports Routes
      */
-    Route::get('payments', ['as' => 'reports.payments.index', 'uses' => 'ReportsController@monthly']);
-    Route::get('payments/daily', ['as' => 'reports.payments.daily', 'uses' => 'ReportsController@daily']);
-    Route::get('payments/monthly', ['as' => 'reports.payments.monthly', 'uses' => 'ReportsController@monthly']);
-    Route::get('payments/yearly', ['as' => 'reports.payments.yearly', 'uses' => 'ReportsController@yearly']);
-    Route::get('current-credits', ['as' => 'reports.current-credits', 'uses' => 'ReportsController@currentCredits']);
+    Route::get('payments', 'ReportsController@monthly')->name('reports.payments.index');
+    Route::get('payments/daily', 'ReportsController@daily')->name('reports.payments.daily');
+    Route::get('payments/monthly', 'ReportsController@monthly')->name('reports.payments.monthly');
+    Route::get('payments/yearly', 'ReportsController@yearly')->name('reports.payments.yearly');
+    Route::get('current-credits', 'ReportsController@currentCredits')->name('reports.current-credits');
 
+    /*
+     * Log Files Routes
+     */
     Route::get('log-files', 'Reports\LogFileController@index')->name('log-files.index');
     Route::get('log-files/{fileName}', 'Reports\LogFileController@show')->name('log-files.show');
     Route::get('log-files/{fileName}/download', 'Reports\LogFileController@download')->name('log-files.download');

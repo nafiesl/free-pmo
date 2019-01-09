307ff909381e94761baff7f5dd26e0fefe1ecaba
Nafies Luthfi <nafiesl@gmail.com>
2019-01-09 20:56:27 +0800

    Move log file show and download to controller

--- app/Http/Controllers/Reports/LogFileController.php
+++ app/Http/Controllers/Reports/LogFileController.php
@@ -21,4 +21,22 @@ class LogFileController extends Controller
 
         return view('reports.log-files', compact('logFiles'));
     }
+
+    public function show($fileName)
+    {
+        if (file_exists(storage_path('logs/'.$fileName))) {
+            return response()->file(storage_path('logs/'.$fileName), ['content-type' => 'text/plain']);
+        }
+
+        return 'Invalid file name.';
+    }
+
+    public function download($fileName)
+    {
+        if (file_exists(storage_path('logs/'.$fileName))) {
+            return response()->download(storage_path('logs/'.$fileName), env('APP_ENV').'.'.$fileName);
+        }
+
+        return 'Invalid file name.';
+    }
 }
--- routes/web/reports.php
+++ routes/web/reports.php
@@ -11,20 +11,6 @@ Route::group(['middleware' => ['web', 'role:admin'], 'prefix' => 'reports'], fun
     Route::get('current-credits', ['as' => 'reports.current-credits', 'uses' => 'ReportsController@currentCredits']);
 
     Route::get('log-files', ['as' => 'log-files.index', 'uses' => 'Reports\LogFileController@index']);
-
-    Route::get('log-files/{filename}', ['as' => 'log-files.show', 'uses' => function ($fileName) {
-        if (file_exists(storage_path('logs/'.$fileName))) {
-            return response()->file(storage_path('logs/'.$fileName), ['content-type' => 'text/plain']);
-        }
-
-        return 'Invalid file name.';
-    }]);
-
-    Route::get('log-files/{filename}/download', ['as' => 'log-files.download', 'uses' => function ($fileName) {
-        if (file_exists(storage_path('logs/'.$fileName))) {
-            return response()->download(storage_path('logs/'.$fileName), env('APP_ENV').'.'.$fileName);
-        }
-
-        return 'Invalid file name.';
-    }]);
+    Route::get('log-files/{filename}', ['as' => 'log-files.show', 'uses' => 'Reports\LogFileController@show']);
+    Route::get('log-files/{filename}/download', ['as' => 'log-files.download', 'uses' => 'Reports\LogFileController@download']);
 });

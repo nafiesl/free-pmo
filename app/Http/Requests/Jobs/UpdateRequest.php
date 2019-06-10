20e87b1092918fcce50bdeb13c0e55b7e971fa75
Damilola Olowookere <damms005@gmail.com>
2019-06-10 20:27:25 +0100

    Remove limit for UpdateRequest handler rules

--- app/Http/Requests/Jobs/UpdateRequest.php
+++ app/Http/Requests/Jobs/UpdateRequest.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Requests\Jobs;
 
-use App\Http\Requests\Request;
 use App\Entities\Projects\Project;
+use App\Http\Requests\Request;
 
 class UpdateRequest extends Request
 {
@@ -26,12 +26,22 @@ class UpdateRequest extends Request
      */
     public function rules()
     {
-        return [
-            'name'        => 'required|max:60',
-            'price'       => 'required|numeric',
-            'worker_id'   => 'required|numeric',
-            'type_id'     => 'required|numeric',
-            'description' => 'max:255',
+        $rules = [
+            'name'      => 'required|max:60',
+            'price'     => 'required|numeric',
+            'worker_id' => 'required|numeric',
+            'type_id'   => 'required|numeric',
         ];
+
+        //Allow for flexibility instead of optionless hard-coded value for "description". This is
+        //achieved using environmental variable.
+        //A value of zero (0) will mean "no limit"
+
+        $char_len_job_description = intval(env("CHAR_LEN_JOB_DESCRIPTION", 255));
+        if ($char_len_job_description > 0) {
+            $rules["description"] = "max:$char_len_job_description";
+        }
+
+        return $rules;
     }
 }

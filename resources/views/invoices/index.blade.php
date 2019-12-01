d063d427fa9a6037b33ebbae5e61089f3a217bc0
Nafies Luthfi <nafiesl@gmail.com>
2019-12-01 20:18:34 +0800

    Improve invoice list page

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -1,30 +1,30 @@
 @extends('layouts.app')
 
-@section('title', trans('invoice.list'))
+@section('title', __('invoice.list'))
 
 @section('content')
 
 <h1 class="page-header">
     <div class="pull-right">
-        {!! FormField::formButton(['route' => 'invoice-drafts.create'], trans('invoice.create'), [
+        {!! FormField::formButton(['route' => 'invoice-drafts.create'], __('invoice.create'), [
             'class' => 'btn btn-success',
             'name' => 'create-invoice-draft',
             'id' => 'invoice-draft-create-button'
         ]) !!}
     </div>
-    {{ trans('invoice.list') }}
+    {{ __('invoice.list') }}
 </h1>
 
 <div class="panel panel-default">
     <table class="table table-condensed">
         <thead>
-            <th class="text-center">{{ trans('app.table_no') }}</th>
-            <th class="col-md-1 text-center">{{ trans('invoice.number') }}</th>
-            <th class="col-md-1 text-center">{{ trans('app.date') }}</th>
-            <th class="col-md-3">{{ trans('project.project') }}</th>
-            <th class="col-md-3">{{ trans('invoice.customer') }}</th>
-            <th class="col-md-2 text-right">{{ trans('invoice.amount') }}</th>
-            <th class="col-md-2 text-center">{{ trans('app.action') }}</th>
+            <th class="text-center">{{ __('app.table_no') }}</th>
+            <th class="col-md-1 text-center">{{ __('invoice.number') }}</th>
+            <th class="col-md-1 text-center">{{ __('app.date') }}</th>
+            <th class="col-md-3">{{ __('project.project') }}</th>
+            <th class="col-md-3">{{ __('invoice.customer') }}</th>
+            <th class="col-md-2 text-right">{{ __('invoice.amount') }}</th>
+            <th class="col-md-2 text-center">{{ __('app.action') }}</th>
         </thead>
         <tbody>
             @forelse($invoices as $key => $invoice)
@@ -49,22 +49,16 @@
                         [
                             'icon' => 'print',
                             'class' => 'btn btn-default btn-xs',
-                            'title' => trans('invoice.print'), 'target' => '_blank'
+                            'title' => __('invoice.print'), 'target' => '_blank'
                         ]
                     ) !!}
                 </td>
             </tr>
             @empty
-            <tr><td colspan="7">{{ trans('invoice.empty') }}</td></tr>
+            <tr><td colspan="7">{{ __('invoice.empty') }}</td></tr>
             @endforelse
         </tbody>
-        <tfoot>
-            <tr>
-                <th colspan="5" class="text-right">{{ trans('app.total') }}</th>
-                <th class="text-right">{{ format_money($invoices->sum('amount')) }}</th>
-                <th>&nbsp;</th>
-            </tr>
-        </tfoot>
     </table>
 </div>
+{{ $invoices->appends(Request::except('page'))->render() }}
 @endsection

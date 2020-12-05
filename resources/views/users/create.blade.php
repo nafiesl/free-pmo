0664db726299b90d8bb7f8678c3c36819057d53c
Nafies Luthfi <nafiesl@gmail.com>
2020-12-05 20:15:26 +0800

    Change trans to __() helper

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,21 +1,21 @@
 @inject('roles', 'App\Entities\Users\Role')
 @extends('layouts.dashboard')
 
-@section('title', trans('user.create'))
+@section('title', __('user.create'))
 
 @section('content-dashboard')
 <div class="row">
     <div class="col-md-6 col-md-offset-2">
         {!! Form::open(['route'=>'users.store']) !!}
         <div class="panel panel-default">
-            <div class="panel-heading"><h3 class="panel-title">{{ trans('user.create') }}</h3></div>
+            <div class="panel-heading"><h3 class="panel-title">{{ __('user.create') }}</h3></div>
             <div class="panel-body">
-                {!! FormField::text('name', ['label' => trans('app.name')]) !!}
-                {!! FormField::email('email', ['label' => trans('user.email')]) !!}
-                {!! FormField::checkboxes('role', $roles::toArray(), ['label' => trans('user.role')]) !!}
+                {!! FormField::text('name', ['label' => __('app.name')]) !!}
+                {!! FormField::email('email', ['label' => __('user.email')]) !!}
+                {!! FormField::checkboxes('role', $roles::toArray(), ['label' => __('user.role')]) !!}
 
                 {!! FormField::password('password', [
-                    'label' => trans('auth.password'),
+                    'label' => __('auth.password'),
                     'info' => [
                         'text' => __('user.create_password_info'),
                         'class' => 'info',
@@ -23,8 +23,8 @@
                 ]) !!}
             </div>
             <div class="panel-footer">
-                {!! Form::submit(trans('user.create'), ['class'=>'btn btn-primary']) !!}
-                {!! link_to_route('users.index', trans('app.cancel'), [], ['class'=>'btn btn-default']) !!}
+                {!! Form::submit(__('user.create'), ['class'=>'btn btn-primary']) !!}
+                {!! link_to_route('users.index', __('app.cancel'), [], ['class'=>'btn btn-default']) !!}
             </div>
         </div>
         {!! Form::close() !!}
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,10 +1,10 @@
 @inject('roles', 'App\Entities\Users\Role')
 @extends('layouts.user')
 
-@section('subtitle', trans('user.edit'))
+@section('subtitle', __('user.edit'))
 
 @section('action-buttons')
-{!! link_to_route('users.show', trans('user.back_to_show'), [$user->id], ['class' => 'btn btn-default']) !!}
+{!! link_to_route('users.show', __('user.back_to_show'), [$user->id], ['class' => 'btn btn-default']) !!}
 @endsection
 
 @section('content-user')
@@ -14,27 +14,27 @@
         <div class="panel panel-default">
             <div class="panel-heading"><h3 class="panel-title">@yield('subtitle')</h3></div>
             <div class="panel-body">
-                {!! FormField::text('name', ['label' => trans('app.name')]) !!}
-                {!! FormField::email('email', ['label' => trans('user.email')]) !!}
+                {!! FormField::text('name', ['label' => __('app.name')]) !!}
+                {!! FormField::email('email', ['label' => __('user.email')]) !!}
                 {!! FormField::checkboxes('role', $roles::toArray(), [
-                    'label' => trans('user.role'),
+                    'label' => __('user.role'),
                     'value' => $user->roles->pluck('role_id')
                 ]) !!}
 
                 {!! FormField::password('password', [
-                    'label' => trans('auth.password'),
+                    'label' => __('auth.password'),
                     'info' => [
                         'text' => __('user.update_password_info'),
                         'class' => 'info',
                     ],
                 ]) !!}
-                {!! FormField::radios('lang', $langList, ['label' => trans('lang.lang')]) !!}
+                {!! FormField::radios('lang', $langList, ['label' => __('lang.lang')]) !!}
             </div>
             <div class="panel-footer">
-                {!! Form::submit(trans('user.update'), ['class'=>'btn btn-success']) !!}
-                {!! link_to_route('users.show', trans('app.cancel'), [$user->id], ['class' => 'btn btn-default']) !!}
+                {!! Form::submit(__('user.update'), ['class'=>'btn btn-success']) !!}
+                {!! link_to_route('users.show', __('app.cancel'), [$user->id], ['class' => 'btn btn-default']) !!}
                 @can('delete', $user)
-                {!! link_to_route('users.delete', trans('app.delete'), [$user->id], ['class'=>'btn btn-danger pull-right']) !!}
+                {!! link_to_route('users.delete', __('app.delete'), [$user->id], ['class'=>'btn btn-danger pull-right']) !!}
                 @endcan
             </div>
         </div>

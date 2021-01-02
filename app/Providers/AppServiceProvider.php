d1dd6ad3ce8a2d5329a8b56f235158075e01563e
Nafies Luthfi <nafiesl@gmail.com>
2021-01-02 08:59:52 +0800

    Use bootstrap paginator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
             'issues'   => 'App\Entities\Projects\Issue',
             'jobs'     => 'App\Entities\Projects\Job',
         ]);
+        Paginator::useBootstrap();
     }
 
     /**

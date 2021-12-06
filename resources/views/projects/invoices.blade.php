b37b22723b9e11ac8a8e67276a9260e900375c0c
Nafies Luthfi <nafiesl@gmail.com>
2021-12-06 16:59:30 +0800

    fix: Update project subscription list page title

--- resources/views/projects/invoices.blade.php
+++ resources/views/projects/invoices.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
-@section('title', trans('project.invoices') . ' | ' . $project->name)
+@section('title', __('project.invoices').' | '.$project->name)
 
 @section('content')
-@include('projects.partials.breadcrumb', ['title' => trans('project.invoices')])
+@include('projects.partials.breadcrumb', ['title' => __('project.invoices')])
 
 <h1 class="page-header">
     <div class="pull-right">
-        {!! FormField::formButton(['route' => 'invoice-drafts.create'], trans('invoice.create'), [
+        {!! FormField::formButton(['route' => 'invoice-drafts.create'], __('invoice.create'), [
             'class' => 'btn btn-success',
             'name' => 'create-invoice-draft',
             'id' => 'invoice-draft-create-button'
@@ -15,22 +15,22 @@
             'project_id' => $project->id
         ]) !!}
     </div>
-    {{ $project->name }} <small>{{ trans('project.invoices') }}</small>
+    {{ $project->name }} <small>{{ __('project.invoices') }}</small>
 </h1>
 
 @include('projects.partials.nav-tabs')
 
 <div class="panel panel-default">
-    <div class="panel-heading"><h3 class="panel-title">{{ trans('project.invoices') }}</h3></div>
+    <div class="panel-heading"><h3 class="panel-title">{{ __('project.invoices') }}</h3></div>
     <table class="table">
         <thead>
-            <th class="text-center">{{ trans('app.table_no') }}</th>
-            <th class="col-md-2 text-center">{{ trans('invoice.number') }}</th>
-            <th class="col-md-2 text-center">{{ trans('invoice.date') }}</th>
-            <th class="col-md-2 text-center">{{ trans('invoice.due_date') }}</th>
-            <th class="col-md-3">{{ trans('invoice.customer') }}</th>
-            <th class="col-md-2 text-right">{{ trans('invoice.amount') }}</th>
-            <th class="col-md-1 text-center">{{ trans('app.action') }}</th>
+            <th class="text-center">{{ __('app.table_no') }}</th>
+            <th class="col-md-2 text-center">{{ __('invoice.number') }}</th>
+            <th class="col-md-2 text-center">{{ __('invoice.date') }}</th>
+            <th class="col-md-2 text-center">{{ __('invoice.due_date') }}</th>
+            <th class="col-md-3">{{ __('invoice.customer') }}</th>
+            <th class="col-md-2 text-right">{{ __('invoice.amount') }}</th>
+            <th class="col-md-1 text-center">{{ __('app.action') }}</th>
         </thead>
         <tbody>
             @forelse($invoices as $key => $invoice)
@@ -62,12 +62,12 @@
                 </td>
             </tr>
             @empty
-            <tr><td colspan="7">{{ trans('invoice.empty') }}</td></tr>
+            <tr><td colspan="7">{{ __('invoice.empty') }}</td></tr>
             @endforelse
         </tbody>
         <tfoot>
             <tr>
-                <th colspan="5" class="text-right">{{ trans('app.total') }}</th>
+                <th colspan="5" class="text-right">{{ __('app.total') }}</th>
                 <th class="text-right">{{ format_money($project->invoices->sum('amount')) }}</th>
                 <th>&nbsp;</th>
             </tr>
--- resources/views/projects/subscriptions.blade.php
+++ resources/views/projects/subscriptions.blade.php
@@ -1,29 +1,29 @@
 @extends('layouts.app')
 
-@section('title', trans('project.subscriptions'))
+@section('title', __('project.subscriptions').' | '.$project->name)
 
 @section('content')
 
-@include('projects.partials.breadcrumb',['title' => trans('project.subscriptions')])
+@include('projects.partials.breadcrumb',['title' => __('project.subscriptions')])
 
 <h1 class="page-header">
     <div class="pull-right">
-        {!! link_to_route('subscriptions.create', trans('subscription.create'), ['project_id' => $project->id, 'customer_id' => $project->customer_id], ['class'=>'btn btn-success']) !!}
+        {!! link_to_route('subscriptions.create', __('subscription.create'), ['project_id' => $project->id, 'customer_id' => $project->customer_id], ['class'=>'btn btn-success']) !!}
     </div>
-    {{ $project->name }} <small>{{ trans('project.subscriptions') }}</small>
+    {{ $project->name }} <small>{{ __('project.subscriptions') }}</small>
 </h1>
 
 @include('projects.partials.nav-tabs')
 
 <table class="table table-condensed">
     <thead>
-        <th>{{ trans('app.table_no') }}</th>
-        <th class="text-center">{{ trans('subscription.type') }}</th>
-        <th>{{ trans('subscription.subscription') }}</th>
-        <th class="text-right">{{ trans('subscription.start_date') }}</th>
-        <th class="text-right">{{ trans('subscription.due_date') }}</th>
-        <th class="text-right">{{ trans('subscription.extension_price') }}</th>
-        <th>{{ trans('app.action') }}</th>
+        <th>{{ __('app.table_no') }}</th>
+        <th class="text-center">{{ __('subscription.type') }}</th>
+        <th>{{ __('subscription.subscription') }}</th>
+        <th class="text-right">{{ __('subscription.start_date') }}</th>
+        <th class="text-right">{{ __('subscription.due_date') }}</th>
+        <th class="text-right">{{ __('subscription.extension_price') }}</th>
+        <th>{{ __('app.action') }}</th>
     </thead>
     <tbody>
         @foreach($project->subscriptions as $key => $subscription)
@@ -35,8 +35,8 @@
             <td class="text-right">{{ date_id($subscription->due_date) }} {!! $subscription->nearOfDueDateSign() !!}</td>
             <td class="text-right">{{ format_money($subscription->price) }}</td>
             <td>
-                {!! link_to_route('subscriptions.show',trans('app.show'),[$subscription->id],['class'=>'btn btn-info btn-xs']) !!}
-                {!! link_to_route('subscriptions.edit',trans('app.edit'),[$subscription->id],['class'=>'btn btn-warning btn-xs']) !!}
+                {!! link_to_route('subscriptions.show',__('app.show'),[$subscription->id],['class'=>'btn btn-info btn-xs']) !!}
+                {!! link_to_route('subscriptions.edit',__('app.edit'),[$subscription->id],['class'=>'btn btn-warning btn-xs']) !!}
             </td>
         </tr>
         @endforeach

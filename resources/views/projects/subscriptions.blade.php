70ee7fbf5e98acaf32305c32519fcf4539b95ac8
Nafies Luthfi <nafiesl@gmail.com>
2022-06-06 22:04:07 +0800

    feat: Add table border and make it responsive on mobile view

--- resources/views/projects/subscriptions.blade.php
+++ resources/views/projects/subscriptions.blade.php
@@ -15,75 +15,81 @@
 
 @include('projects.partials.nav-tabs')
 
-<h3>{{ __('app.active') }}</h3>
+<div class="panel panel-default">
+    <div class="panel-heading"><h3 class="panel-title">{{ __('app.active') }}</h3></div>
+    <div class="panel-body table-responsive">
+        <table class="table table-condensed">
+            <thead>
+                <th>{{ __('app.table_no') }}</th>
+                <th class="text-center">{{ __('app.type') }}</th>
+                <th>{{ __('subscription.subscription') }}</th>
+                <th class="text-center">{{ __('app.status') }}</th>
+                <th class="text-right">{{ __('subscription.start_date') }}</th>
+                <th class="text-right">{{ __('subscription.due_date') }}</th>
+                <th class="text-right">{{ __('subscription.extension_price') }}</th>
+                <th>{{ __('app.action') }}</th>
+            </thead>
+            <tbody>
+                @foreach($activeSbscriptions as $key => $subscription)
+                <tr>
+                    <td>{{ 1 + $key }}</td>
+                    <td class="text-center">{{ $subscription->type }}</td>
+                    <td>{{ $subscription->name_link }}</td>
+                    <td class="text-center">{{ $subscription->status() }}</td>
+                    <td class="text-right">{{ date_id($subscription->start_date) }}</td>
+                    <td class="text-right">{{ date_id($subscription->due_date) }} {!! $subscription->nearOfDueDateSign() !!}</td>
+                    <td class="text-right">{{ format_money($subscription->price) }}</td>
+                    <td>
+                        {!! link_to_route('subscriptions.show',__('app.show'),[$subscription->id],['class'=>'btn btn-info btn-xs']) !!}
+                        {!! link_to_route('subscriptions.edit',__('app.edit'),[$subscription->id],['class'=>'btn btn-warning btn-xs']) !!}
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th colspan="6" class="text-right">{{ __('app.total') }}</th>
+                    <th class="text-right">{{ format_money($activeSbscriptions->sum('price')) }}</th>
+                    <th>&nbsp;</th>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+</div>
 
-<table class="table table-condensed">
-    <thead>
-        <th>{{ __('app.table_no') }}</th>
-        <th class="text-center">{{ __('app.type') }}</th>
-        <th>{{ __('subscription.subscription') }}</th>
-        <th class="text-center">{{ __('app.status') }}</th>
-        <th class="text-right">{{ __('subscription.start_date') }}</th>
-        <th class="text-right">{{ __('subscription.due_date') }}</th>
-        <th class="text-right">{{ __('subscription.extension_price') }}</th>
-        <th>{{ __('app.action') }}</th>
-    </thead>
-    <tbody>
-        @foreach($activeSbscriptions as $key => $subscription)
-        <tr>
-            <td>{{ 1 + $key }}</td>
-            <td class="text-center">{{ $subscription->type }}</td>
-            <td>{{ $subscription->name_link }}</td>
-            <td class="text-center">{{ $subscription->status() }}</td>
-            <td class="text-right">{{ date_id($subscription->start_date) }}</td>
-            <td class="text-right">{{ date_id($subscription->due_date) }} {!! $subscription->nearOfDueDateSign() !!}</td>
-            <td class="text-right">{{ format_money($subscription->price) }}</td>
-            <td>
-                {!! link_to_route('subscriptions.show',__('app.show'),[$subscription->id],['class'=>'btn btn-info btn-xs']) !!}
-                {!! link_to_route('subscriptions.edit',__('app.edit'),[$subscription->id],['class'=>'btn btn-warning btn-xs']) !!}
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-    <tfoot>
-        <tr>
-            <th colspan="6" class="text-right">{{ __('app.total') }}</th>
-            <th class="text-right">{{ format_money($activeSbscriptions->sum('price')) }}</th>
-            <th>&nbsp;</th>
-        </tr>
-    </tfoot>
-</table>
-
-<h3>{{ __('app.in_active') }}</h3>
-
-<table class="table table-condensed">
-    <thead>
-        <th>{{ __('app.table_no') }}</th>
-        <th class="text-center">{{ __('app.type') }}</th>
-        <th>{{ __('subscription.subscription') }}</th>
-        <th class="text-center">{{ __('app.status') }}</th>
-        <th class="text-right">{{ __('subscription.start_date') }}</th>
-        <th class="text-right">{{ __('subscription.due_date') }}</th>
-        <th class="text-right">{{ __('subscription.extension_price') }}</th>
-        <th>{{ __('app.action') }}</th>
-    </thead>
-    <tbody>
-        @foreach($inactiveSbscriptions as $key => $subscription)
-        <tr>
-            <td>{{ 1 + $key }}</td>
-            <td class="text-center">{{ $subscription->type }}</td>
-            <td>{{ $subscription->name_link }}</td>
-            <td class="text-center">{{ $subscription->status() }}</td>
-            <td class="text-right">{{ date_id($subscription->start_date) }}</td>
-            <td class="text-right">{{ date_id($subscription->due_date) }} {!! $subscription->nearOfDueDateSign() !!}</td>
-            <td class="text-right">{{ format_money($subscription->price) }}</td>
-            <td>
-                {!! link_to_route('subscriptions.show',__('app.show'),[$subscription->id],['class'=>'btn btn-info btn-xs']) !!}
-                {!! link_to_route('subscriptions.edit',__('app.edit'),[$subscription->id],['class'=>'btn btn-warning btn-xs']) !!}
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="panel panel-default">
+    <div class="panel-heading"><h3 class="panel-title">{{ __('app.in_active') }}</h3></div>
+    <div class="panel-body table-responsive">
+        <table class="table table-condensed">
+            <thead>
+                <th>{{ __('app.table_no') }}</th>
+                <th class="text-center">{{ __('app.type') }}</th>
+                <th>{{ __('subscription.subscription') }}</th>
+                <th class="text-center">{{ __('app.status') }}</th>
+                <th class="text-right">{{ __('subscription.start_date') }}</th>
+                <th class="text-right">{{ __('subscription.due_date') }}</th>
+                <th class="text-right">{{ __('subscription.extension_price') }}</th>
+                <th>{{ __('app.action') }}</th>
+            </thead>
+            <tbody>
+                @foreach($inactiveSbscriptions as $key => $subscription)
+                <tr>
+                    <td>{{ 1 + $key }}</td>
+                    <td class="text-center">{{ $subscription->type }}</td>
+                    <td>{{ $subscription->name_link }}</td>
+                    <td class="text-center">{{ $subscription->status() }}</td>
+                    <td class="text-right">{{ date_id($subscription->start_date) }}</td>
+                    <td class="text-right">{{ date_id($subscription->due_date) }} {!! $subscription->nearOfDueDateSign() !!}</td>
+                    <td class="text-right">{{ format_money($subscription->price) }}</td>
+                    <td>
+                        {!! link_to_route('subscriptions.show',__('app.show'),[$subscription->id],['class'=>'btn btn-info btn-xs']) !!}
+                        {!! link_to_route('subscriptions.edit',__('app.edit'),[$subscription->id],['class'=>'btn btn-warning btn-xs']) !!}
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 
 @endsection

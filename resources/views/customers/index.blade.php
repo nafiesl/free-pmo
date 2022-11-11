85bbed37f9476915305957d75c5c46e58b7e8ee8
Nafies Luthfi <nafiesl@gmail.com>
2022-11-11 23:50:16 +0800

    fix: Customer list table responsive

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -11,42 +11,44 @@
     <small>{{ __('app.total') }} : {{ $customers->total() }} {{ __('customer.customer') }}</small>
 </h1>
 
-<div class="panel panel-default table-responsive">
+<div class="panel panel-default">
     <div class="panel-heading">
         {{ Form::open(['method' => 'get', 'class' => 'form-inline']) }}
         {!! FormField::text('q', ['value' => request('q'), 'label' => __('customer.search'), 'class' => 'input-sm']) !!}
-        {{ Form::submit(__('customer.search'), ['class' => 'btn btn-sm']) }}
+        {{ Form::submit(__('customer.search'), ['class' => 'btn btn-info btn-sm']) }}
         {{ link_to_route('customers.index', __('app.reset')) }}
         {{ Form::close() }}
     </div>
-    <table class="table table-condensed">
-        <thead>
-            <tr>
-                <th class="text-center">{{ __('app.table_no') }}</th>
-                <th>{{ __('customer.name') }}</th>
-                <th>{{ __('contact.email') }}</th>
-                <th>{{ __('contact.phone') }}</th>
-                <th class="text-center">{{ __('customer.projects_count') }}</th>
-                <th class="text-center">{{ __('app.status') }}</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($customers as $key => $customer)
-            <tr>
-                <td class="text-center">{{ $customers->firstItem() + $key }}</td>
-                <td>{{ $customer->nameLink() }}</td>
-                <td>{{ $customer->email }}</td>
-                <td>{{ $customer->phone }}</td>
-                <td class="text-center">
-                    @if ($customer->projects_count)
-                    {{ link_to_route('customers.projects', $customer->projects_count, $customer) }}
-                    @endif
-                </td>
-                <td class="text-center">{!! $customer->status_label !!}</td>
+    <div class="table-responsive">
+        <table class="table table-condensed">
+            <thead>
+                <tr>
+                    <th class="text-center">{{ __('app.table_no') }}</th>
+                    <th>{{ __('customer.name') }}</th>
+                    <th>{{ __('contact.email') }}</th>
+                    <th>{{ __('contact.phone') }}</th>
+                    <th class="text-center">{{ __('customer.projects_count') }}</th>
+                    <th class="text-center">{{ __('app.status') }}</th>
                 </tr>
-                @endforeach
-            </tbody>
-        </table>
+            </thead>
+            <tbody>
+                @foreach($customers as $key => $customer)
+                <tr>
+                    <td class="text-center">{{ $customers->firstItem() + $key }}</td>
+                    <td>{{ $customer->nameLink() }}</td>
+                    <td>{{ $customer->email }}</td>
+                    <td>{{ $customer->phone }}</td>
+                    <td class="text-center">
+                        @if ($customer->projects_count)
+                        {{ link_to_route('customers.projects', $customer->projects_count, $customer) }}
+                        @endif
+                    </td>
+                    <td class="text-center">{!! $customer->status_label !!}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
         <div class="panel-body">{{ $customers->appends(Request::except('page'))->render() }}</div>
     </div>
 </div>

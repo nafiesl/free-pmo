8636d5e3c1de8d5d8c8f311aef1b00c9a9367999
Nafies Luthfi <nafiesl@gmail.com>
2022-11-11 23:47:33 +0800

    fix: Vendor list table responseive

--- resources/views/vendors/index.blade.php
+++ resources/views/vendors/index.blade.php
@@ -12,42 +12,44 @@
 </h1>
 <div class="row">
     <div class="col-md-8">
-        <div class="panel panel-default table-responsive">
+        <div class="panel panel-default">
             <div class="panel-heading">
                 {{ Form::open(['method' => 'get', 'class' => 'form-inline']) }}
                 {!! FormField::text('q', ['value' => request('q'), 'label' => __('vendor.search'), 'class' => 'input-sm']) !!}
-                {{ Form::submit(__('vendor.search'), ['class' => 'btn btn-sm']) }}
+                {{ Form::submit(__('vendor.search'), ['class' => 'btn btn-info btn-sm']) }}
                 {{ link_to_route('vendors.index', __('app.reset')) }}
                 {{ Form::close() }}
             </div>
-            <table class="table table-condensed">
-                <thead>
-                    <tr>
-                        <th class="text-center">{{ __('app.table_no') }}</th>
-                        <th>{{ __('vendor.name') }}</th>
-                        <th>{{ __('app.notes') }}</th>
-                        <th class="text-center">{{ __('app.action') }}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($vendors as $key => $vendor)
-                    <tr>
-                        <td class="text-center">{{ $vendors->firstItem() + $key }}</td>
-                        <td>{{ $vendor->name }}</td>
-                        <td>{{ $vendor->notes }}</td>
-                        <td class="text-center">
-                            {{ link_to_route('vendors.show', __('app.show'), $vendor) }} |
-                            {{ link_to_route(
-                                'vendors.index',
-                                __('app.edit'),
-                                ['action' => 'edit', 'id' => $vendor->id] + request(['page', 'q']),
-                                ['id' => 'edit-vendor-' . $vendor->id]
-                            ) }}
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table table-condensed">
+                    <thead>
+                        <tr>
+                            <th class="text-center">{{ __('app.table_no') }}</th>
+                            <th>{{ __('vendor.name') }}</th>
+                            <th>{{ __('app.notes') }}</th>
+                            <th class="text-center">{{ __('app.action') }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($vendors as $key => $vendor)
+                        <tr>
+                            <td class="text-center">{{ $vendors->firstItem() + $key }}</td>
+                            <td>{{ $vendor->name }}</td>
+                            <td>{{ $vendor->notes }}</td>
+                            <td class="text-center">
+                                {{ link_to_route('vendors.show', __('app.show'), $vendor) }} |
+                                {{ link_to_route(
+                                    'vendors.index',
+                                    __('app.edit'),
+                                    ['action' => 'edit', 'id' => $vendor->id] + request(['page', 'q']),
+                                    ['id' => 'edit-vendor-' . $vendor->id]
+                                ) }}
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
             <div class="panel-body">{{ $vendors->appends(Request::except('page'))->render() }}</div>
         </div>
     </div>
